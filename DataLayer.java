3e00ecd183b8ef58c8b975a1b79d2ce8988dcf56
Jeriel Ng <jeriel.ng@gmail.com>
2017-10-05 14:36:18 -0500

    Added Java Files

---
+++ DataLayer.java
@@ -0,0 +1,20 @@
+public class DataLayer {
+   public class User {
+   
+   }
+   
+   public class Customer {
+   }
+   
+   public class Manager {
+   }
+   
+   public class Transaction {
+   }
+   
+   public class Inventory {
+   }
+   
+   public class Payment {
+   }
+}
---
+++ ViewLayer.java
@@ -0,0 +1,26 @@
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+
+public class ViewLayer extends JFrame {
+   JFrame frame = new JFrame("FrameDemo");
+   
+   public static void main(String[] args) {
+      
+   }
+   
+   public class ScreenView {
+   }
+   
+   public class Form {
+   }
+   
+   public class Image {
+   }
+   
+   public class Button {
+   }
+   
+   public class TableView {
+   }
+}
+
