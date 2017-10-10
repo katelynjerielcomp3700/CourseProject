5388f9a29541bfa32f242740b15f9ec1bd1aca24
Jeriel Ng <jeriel.ng@gmail.com>
2017-10-10 09:58:04 -0500

    Modified Java classes

--- ViewLayer.java
+++ ViewLayer.java
@@ -1,11 +1,17 @@
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import java.awt.BorderLayout;
 
 public class ViewLayer extends JFrame {
-   JFrame frame = new JFrame("FrameDemo");
    
    public static void main(String[] args) {
-      
+      JFrame frame = new JFrame("FrameDemo");
+      JLabel emptyLabel = new JLabel();
+      frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+      frame.getContentPane().add(emptyLabel, BorderLayout.CENTER);
+      frame.setSize(1200, 800);
+      //frame.pack();
+      frame.setVisible(true);
    }
    
    public class ScreenView {
