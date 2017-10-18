d791d46a105f4cedf5109848a6a2e2930614426a
katelynjerielcomp3700 <31670613+katelynjerielcomp3700@users.noreply.github.com>
2017-10-18 15:43:55 -0500

    Add files via upload

---
+++ Order.java
@@ -0,0 +1,48 @@
+public class Order {
+    private int ID;
+    private String paymentType;
+    private double amount;
+    private String date;
+    private String storeInfo;
+    
+
+    public double getAmount() {
+        return amount;
+    }
+
+    public void setAmount(double amount) {
+        this.amount = amount;
+    }
+
+    public int getID() {
+        return ID;
+    }
+
+    public void setID(int ID) {
+        this.ID = ID;
+    }
+
+    public String getPaymentType() {
+        return paymentType;
+    }
+
+    public void setPaymentType(String paymentType) {
+        this.paymentType = paymentType;
+    }
+    
+        public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+    
+        public String getStoreInfo() {
+        return storeInfo;
+    }
+
+    public void setStoreInfor(String storeInfo) {
+        this.storeInfo = storeInfo;
+    }
+}
